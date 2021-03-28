a9ee629764fa5a89c9ac216f680c091480b41054
MrozKarol <polityk357@gmail.com>
2021-03-28 19:24:46 +0200

    adding, dynamically changing the highlighting of the bar element navigation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const navbar = document.querySelector(".navbar");
-
 const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.navbar-link')
 
 window.addEventListener('scroll', () => {
     // console.log(window.pageYOffset, navbar.offsetTop)
@@ -11,4 +12,15 @@ window.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('sticky')
     }
+    sections.forEach((section, index) => {
+        if (window.pageYOffset >= section.offsetTop - 10) {
+            //usuwanie podswietlenia
+            navbarLinks.forEach(navbarLinks => {
+                navbarLinks.classList.remove('change')
+            })
+            // dpdawanie podswietlenia
+            navbarLinks[index].classList.add('change')
+        }
+    })
 })
+
