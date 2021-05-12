280de04869e2b90c603eed18c9f6e62595f503a2
MrozKarol <polityk357@gmail.com>
2021-05-12 18:45:51 +0200

    add section 2, business agency project

--- portfolio/04_business _agency_website/script.js
+++ portfolio/04_business _agency_website/script.js
@@ -9,3 +9,39 @@ menu.addEventListener('click', () => {
 })
 
 // end of Navbar 
+
+// sction 2 video 
+
+const video = document.querySelector('.video')
+const btn = document.querySelector('.buttons i')
+const bar = document.querySelector('.video-bar')
+
+
+const playPause = () => {
+    if (video.paused) {
+        video.play()
+        btn.className = 'far fa-pause-circle'
+        video.style.opacity = '.7'
+
+    } else {
+        video.pause()
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3'
+    }
+}
+
+btn.addEventListener('click', playPause)
+
+video.addEventListener('timeupdate', () => {
+    // console.log(video.currentTime, video.duration)
+    // console.log(video.currentTime / video.duration)
+    const barWidth = video.currentTime / video.duration
+    bar.style.width = `${barWidth * 100}%`
+
+    if (video.ended) {
+        btn.className = 'far fa-play-circle'
+        video.style.opacity = '.3s'
+    }
+})
+
+// end of section 2 video 
