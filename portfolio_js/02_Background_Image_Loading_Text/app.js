96dcb79ea573ccc5a9776cd25b71a81744a77b43
MrozKarol <polityk357@gmail.com>
2021-05-18 19:43:14 +0200

    added Blurry Loading

---
+++ portfolio_js/02_Background_Image_Loading_Text/app.js
@@ -0,0 +1,26 @@
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0
+
+const blurring = () => {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    // console.log(load)
+    loadText.textContent = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+
+
+}
+
+const int = setInterval(blurring, 30)
+
+
+
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
