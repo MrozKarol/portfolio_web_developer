1c3460c155c8c36cd7aaa0b56eebe45af3083ccf
MrozKarol <polityk357@gmail.com>
2022-05-13 21:54:39 +0200

    added fetch new quote by button and added tweet by tweeter button

--- portfolio_js/10_Quote_Generator/script.js
+++ portfolio_js/10_Quote_Generator/script.js
@@ -1,10 +1,25 @@
+const quoteContainer = document.getElementById('quote-conteiner');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 //2 Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-  console.log(quote);
+
+  //Check if Aauthor field is black and replace it with "Unkonown"
+  // console.log(quote.author);
+  authorText.textContent = quote.author ? quote.author : 'Unkonown';
+
+  // Check Quote lenght to determine styling
+  quote.text.length > 120
+    ? quoteText.classList.add('long-quote')
+    : quoteText.classList.remove('long-quote');
+  quoteText.textContent = quote.text;
 }
 
 //1 Get Quotes From API
@@ -19,6 +34,17 @@ async function getQuotes() {
     //   Catch Error Here
   }
 }
+
+//4 Tweet Quote
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+};
+
+//5 Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //3 On Load
 getQuotes();
