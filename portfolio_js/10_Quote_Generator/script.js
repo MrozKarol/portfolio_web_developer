9ef26ef877674637d8e33e3796aae1ccaafeb77f
MrozKarol <polityk357@gmail.com>
2022-05-13 22:22:05 +0200

    added Creating A Loader With JavaScript

--- portfolio_js/10_Quote_Generator/script.js
+++ portfolio_js/10_Quote_Generator/script.js
@@ -1,13 +1,27 @@
-const quoteContainer = document.getElementById('quote-conteiner');
+const quoteContainer = document.getElementById('quote-contenier');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+const loading = () => {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+};
+
+// Hide Loading
+const complete = () => {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+};
+
 //2 Show New Quote
 function newQuote() {
+  loading();
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
@@ -19,11 +33,15 @@ function newQuote() {
   quote.text.length > 120
     ? quoteText.classList.add('long-quote')
     : quoteText.classList.remove('long-quote');
+
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 //1 Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const respone = await fetch(apiUrl);
