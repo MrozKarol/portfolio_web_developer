2b86a4f08c9e7d6f2ba790aa2c56817a3c10a25b
MrozKarol <polityk357@gmail.com>
2022-06-29 20:23:40 +0200

    display the button on the calculator display

---
+++ portfolio_js/12_Calculator/script.js
@@ -0,0 +1,21 @@
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+  // If current dispaly value is 0, replace it, if not add number
+  const dispalyValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    dispalyValue === '0' ? number : dispalyValue + number;
+}
+
+// Add Eevent Liseners for numbers, operators, decimal buttons
+inputBtns.forEach((inputBtn) => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('operator')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('decimal')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }
+});
