fbf254b4a780f82ba0c96ad3087a7683b9ea944f
MrozKarol <polityk357@gmail.com>
2022-07-26 21:47:27 +0200

    added next and backward song switch

--- portfolio_js/13_Music_Player/script.js
+++ portfolio_js/13_Music_Player/script.js
@@ -1,10 +1,61 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
-
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('prev');
 
 let isPlaing = false;
+let songIndex = 0;
+
+// Music
+const songs = [
+  {
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
+  },
+  {
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
+];
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+    loadSong(songs[songIndex]);
+    pauseSong();
+  } else {
+    loadSong(songs[songIndex]);
+    playSong();
+  }
+}
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+    loadSong(songs[songIndex]);
+    pauseSong();
+  } else {
+    loadSong(songs[songIndex]);
+    playSong();
+  }
+}
 
 // Play
 function playSong() {
@@ -26,3 +77,20 @@ function pauseSong() {
 playBtn.addEventListener('click', () => {
   isPlaing ? pauseSong() : playSong();
 });
+
+//Update music player/ DOM
+
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// Load song
+
+loadSong(songs[songIndex]);
+
+// Event Leseners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
