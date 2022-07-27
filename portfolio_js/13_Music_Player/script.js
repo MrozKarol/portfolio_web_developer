37a18b347ad50d81a70c889cba3d8c67f2cfcbc7
MrozKarol <polityk357@gmail.com>
2022-07-27 20:16:16 +0200

    added working music progress bar, with updating duration and current time music played

--- portfolio_js/13_Music_Player/script.js
+++ portfolio_js/13_Music_Player/script.js
@@ -5,6 +5,10 @@ const music = document.querySelector('audio');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('prev');
+const progressContainer = document.getElementById('progress-container');
+const progress = document.getElementById('progress');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
 
 let isPlaing = false;
 let songIndex = 0;
@@ -91,6 +95,58 @@ function loadSong(song) {
 
 loadSong(songs[songIndex]);
 
+// Uptade Progress Bar and Time
+function updateProgresBar(e) {
+  if (isPlaing) {
+    const { duration, currentTime } = e.srcElement;
+    // console.log(duration, currentTime);
+    // Updare progress bar width
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+
+    // Calculate display for currentTime
+    const currentMinutes = Math.floor(currentTime / 60);
+    let currentSeconds = Math.floor(currentTime % 60);
+    if (currentSeconds < 10) {
+      currentSeconds = `0${currentSeconds}`;
+    }
+    currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
+  }
+}
+
+// Uptade Time when is not playing
+
+function updateDuration(e) {
+  if (!isPlaing) {
+    const { duration } = e.target;
+    console.log(e);
+    // Calculate display for duration
+    const durationMinutes = Math.floor(duration / 60);
+    let durationSeconds = Math.floor(duration % 60);
+    if (durationSeconds < 10) {
+      durationSeconds = `0${durationSeconds}`;
+    }
+
+    // Delay switching duration Element to avoid NaN
+    if (durationSeconds) {
+      durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+    }
+  }
+}
+
 // Event Leseners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('timeupdate', updateProgresBar);
+music.addEventListener('loadeddata', updateDuration);
