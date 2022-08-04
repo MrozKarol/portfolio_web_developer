e5b5eea2275be262fdfe780c395d0cc4bba7faf4
MrozKarol <polityk357@gmail.com>
2022-08-04 17:14:36 +0200

    Set Date Input Min with Today's Date

---
+++ portfolio_js/14_Custom_Countdown/script.js
@@ -0,0 +1,10 @@
+const imputContainer = document.getElementById('input-conteiner');
+const countdownForm = document.getElementById('countdownForm');
+const dateEl = document.getElementById('date-picker');
+
+//Set Date Input Min with Today's Date
+
+const today = new Date().toISOString().split('T')[0];
+console.log(today);
+
+dateEl.setAttribute('min', today);
