74d3dbc62f122268d72dfbeb94499f0997c56b39
MrozKarol <polityk357@gmail.com>
2023-04-05 20:15:52 +0200

    fix http problem in 07_Movie_App

--- portfolio_js/07_Movie_App/app.js
+++ portfolio_js/07_Movie_App/app.js
@@ -1,27 +1,32 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie/?sort_by=populaty.desc&api_key=86f7c78abc5fc2a1971ef902c51140da&page=30'
+const API_URL = `https://api.themoviedb.org/3/discover/movie/?sort_by=populaty.desc&api_key=86f7c78abc5fc2a1971ef902c51140da&page=${Math.floor(
+  Math.random() * 40 + 1
+)}`;
 
-const IMG_PATH = 'https://image.tmdb.org/t/p/w500'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w500';
 
-const SEARCH_API = 'https://api.themoviedb.org/3/search/movie/?api_key=86f7c78abc5fc2a1971ef902c51140da&query='
+const SEARCH_API =
+  'https://api.themoviedb.org/3/search?movie?api_key=86f7c78abc5fc2a1971ef902c51140da&query=';
 
-const form = document.getElementById('form')
+const form = document.getElementById('form');
 
-const search = document.getElementById('search')
+const search = document.getElementById('search');
 
-const main = document.getElementById('main')
-
-
-
+const main = document.getElementById('main');
 
 const showMovies = (movies) => {
-    main.innerHTML = ''
-    movies.forEach((movie) => {
-        const { title, original_title, poster_path, vote_average, overview, } = movie
-        const movieEl = document.createElement('div')
-        movieEl.classList.add('movie')
+  main.innerHTML = '';
+  movies.forEach((movie) => {
+    const { title, original_title, poster_path, vote_average, overview } =
+      movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
 
-        movieEl.innerHTML = `
-        <img src="${poster_path ? IMG_PATH + poster_path : 'https://images.unsplash.com/photo-1610337673044-720471f83677?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1566&q=80'}"
+    movieEl.innerHTML = `
+        <img src="${
+          poster_path
+            ? IMG_PATH + poster_path
+            : 'https://images.unsplash.com/photo-1610337673044-720471f83677?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1566&q=80'
+        }"
                 alt="movie">
 
             <div class="movie-info">
@@ -32,71 +37,57 @@ const showMovies = (movies) => {
         <h3>Overview</h3>
         ${overview}
     </div>
-`
-        main.appendChild(movieEl)
-        // console.log(movieEl)
-    })
-}
-
+`;
+    main.appendChild(movieEl);
+    // console.log(movieEl)
+  });
+};
 
 const getClassByRate = (vote_average) => {
-    if (vote_average >= 8) {
-        return 'green'
-    } else if (vote_average >= 5) {
-        return 'orange'
-    } else {
-        return 'red'
-    }
-}
+  if (vote_average >= 8) {
+    return 'green';
+  } else if (vote_average >= 5) {
+    return 'orange';
+  } else {
+    return 'red';
+  }
+};
 
 const getMovies = () => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+  };
 
-    const config = {
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8'
-        }
-    }
-
-    fetch(API_URL, config)
-        .then(response => response.json())
-        .then(data =>
-            showMovies(data.results)
-            // console.log(data.results)
-
-        )
-
-}
-
-
-
-
+  fetch(API_URL, config)
+    .then((response) => response.json())
+    .then(
+      (data) => showMovies(data.results)
+      // console.log(data.results)
+    );
+};
 
 const searchMovies = () => {
+  const searchTerm = `'${search.value}'`;
 
-    const searchTerm = `'${search.value}'`
+  console.log(SEARCH_API + searchTerm);
 
-    // console.log(SEARCH_API + searchTerm)
-
-    fetch(SEARCH_API + searchTerm)
-        .then(response => response.json())
-        .then(data =>
-            showMovies(data.results)
-        )
-}
+  fetch(SEARCH_API + searchTerm)
+    .then((response) => response.json())
+    .then((data) => showMovies(data.results));
+};
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const searchTerm = search.value
+  e.preventDefault();
+  const searchTerm = search.value;
 
-    if (searchTerm && searchTerm !== '') {
+  if (searchTerm && searchTerm !== '') {
+    searchMovies();
+    search.value = '';
+  } else {
+    window.location.reload();
+  }
+});
 
-        searchMovies()
-        search.value = ''
-
-    } else {
-        window.location.reload()
-    }
-
-})
-
-getMovies()
+getMovies();
